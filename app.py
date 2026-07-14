8afee556054cf8e1bd0373476d12a5959ada49f9
Ranithri Hewasiliyan <adhewasiliyan@gmail.com>
2026-07-14 19:19:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -322,8 +322,18 @@ st.write("Customer segmentation based on their purchasing behavior.")
 
 st.markdown("""
 <div style="background-color: #f0f2f6; padding: 20px; border-radius: 10px; text-align: center;">
-    <p>Explore customer segments and run live predictions in this interactive dashboard.</p>
     <a href="https://rfm-analysis-by-ranithri.streamlit.app/"_blank" style="text-decoration: none; background-color: #ff4b4b; color: white; padding: 10px 20px; border-radius: 5px; font-weight: bold;">
+        Open Interactive Dashboard ↗    
+    </a>
+</div>
+""", unsafe_allow_html=True)
+
+st.markdown("<h2 class='project-heading'>Tea Crop Prediction</h2>", unsafe_allow_html=True)
+st.write("Tea crop prediction based on historical data and environmental factors.")
+
+st.markdown("""
+<div style="background-color: #f0f2f6; padding: 20px; border-radius: 10px; text-align: center;">
+    <a href="https://tea-crop-by-ranithri.streamlit.app/"_blank" style="text-decoration: none; background-color: #ff4b4b; color: white; padding: 10px 20px; border-radius: 5px; font-weight: bold;">
         Open Interactive Dashboard ↗    
     </a>
 </div>
